ac80d99d504bdc481b1969a85515f46ea1ee34be
mazcona333 <mazcona333@gmail.com>
2024-03-14 16:14:43 +0100

    Add more blargg tests

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -9,6 +9,10 @@ int main(int argc, char const *argv[])
     Gba *gba = new Gba();
     //gba->loadROM("D:\\Git\\GameBoy-Emulator\\roms\\blargg\\cpu_instrs\\individual\\09-op r,r.gb");
     gba->loadROM("D:\\Git\\GameBoy-Emulator\\roms\\blargg\\cpu_instrs\\cpu_instrs.gb");
+    //gba->loadROM("D:\\Git\\GameBoy-Emulator\\roms\\blargg\\instr_timing\\instr_timing.gb");
+    //gba->loadROM("D:\\Git\\GameBoy-Emulator\\roms\\blargg\\interrupt_time\\interrupt_time.gb");
+    //gba->loadROM("D:\\Git\\GameBoy-Emulator\\roms\\blargg\\mem_timing\\mem_timing.gb");
+    //gba->loadROM("D:\\Git\\GameBoy-Emulator\\roms\\blargg\\mem_timing-2\\mem_timing.gb");
 
     float Mhz = 4.194304f;
     float cycleDelay = 1 / (Mhz * 1000000);
